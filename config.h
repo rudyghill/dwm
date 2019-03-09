d657a3b1b8fd2bed288750cdaf3393a534a0ff4c
Rudy Hill <rudyghill@gmail.com>
2019-03-09 09:49:32 -0700

    On branch master  Your branch is ahead of 'origin/master' by 1 commit.    (use "git push" to publish your local commits)
     Changes to be committed:
    	modified:   config.h

--- config.h
+++ config.h
@@ -6,8 +6,8 @@ static const unsigned int gappx     = 5;
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "mono:size=12" };
-static const char dmenufont[]       = "mono:size=12";
+static const char *fonts[]          = { "serif:size=14" };
+static const char dmenufont[]       = "serif:size=14";
 static const char col_gray1[]       = "#000000";
 static const char col_gray2[]       = "#000000";
 static const char col_gray3[]       = "#93a1a1";
@@ -20,7 +20,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -61,7 +61,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon,"-fn",dmenufont,"-p","run:","-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon,"-fn",dmenufont,"-p","execvte:","-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 //static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
@@ -95,7 +95,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_x,      spawn,          SHCMD("slock") },
 	{ MODKEY|ShiftMask,             XK_x,      quit,           {0} },
 	{ MODKEY,                       XK_m,      spawn,          SHCMD("st -e ncmpcpp") },
-	{ MODKEY,                       XK_m,      spawn,          SHCMD("dmenurecord") },
+	{ MODKEY,                       XK_r,      spawn,          SHCMD("dmenurecord") },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
