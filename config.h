6cc89f17d06c05488c23ac6ac277c3b90d2fbc5c
Rudy Hill <rudyghill@gmail.com>
2019-06-15 14:45:38 -0600

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch colors
     Changes to be committed:
    	color palette:   config.h
    	statuscolors:   dwm.c
    	new file:   patches/dwm-statuscolors-20181008-b69c870.diff

--- config.h
+++ config.h
@@ -8,15 +8,14 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=14" };
 static const char dmenufont[]       = "mono:size=14";
-static const char col_gray1[]       = "#000000";
-static const char col_gray2[]       = "#000000";
-static const char col_gray3[]       = "#93a1a1";
-static const char col_gray4[]       = "#000000";
-static const char col_cyan[]        = "#268bd2";
+
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { "#cccccc", "#000000", "#000000"},
+	[SchemeSel]  = { "#ffffff", "#268bd2", "#268bd2"},
+	[SchemeWarn] = { "#dc322f", "#000000", "#000000"},
+	[SchemeUrgent] ={"#b58900", "#000000", "#000000"},
+	[SchemeGood] = { "#859900", "#000000", "#000000"},
 };
 
 /* tagging */
@@ -61,7 +60,8 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon,"-fn",dmenufont,"-p","execvte:","-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+// static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon,"-fn",dmenufont,"-p","execvte:","-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon,"-fn",dmenufont,"-p","execvte:", NULL };
 //static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
